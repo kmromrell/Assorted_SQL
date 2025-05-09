7977e7cb924ff9767e659779c9e20870265d8a6f
Katie Romrell <kmromrell@gmail.com>
2025-05-08 23:36:43 -0700

    Percent of ties by country by season

--- case_practice.sql
+++ case_practice.sql
@@ -1,4 +1,37 @@
--- Identifying the number of matches played by each country during the three different seasons.
+/* The following questions are queried using data collected about soccer matches in Europe. There are three tables in the data set: country (with just the country code to country), teams (giving the team code and long/short names), and matches (with all the match data, including who's playing (by code), home/away score, etc.). This data was then partitioned off by country, resulting in tables for each country (e.g., matches_spain, teams_germany, etc.). The following queries were used to answer the given questions. I've listed the queries in reverse order of difficulty (focusing on the full-data queries first and then looking at the country based ones). */
+
+-- Calculate the percentage of ties tht occur in each country, separated by the 2013-14 and 2014-15 seasons.
+
+SELECT 
+	c.name AS country,
+	ROUND(AVG(CASE WHEN m.season='2013/2014' AND m.home_goal = m.away_goal THEN 1
+			WHEN m.season='2013/2014' AND m.home_goal != m.away_goal THEN 0
+			END), 2) AS pct_ties_2013_2014,
+	ROUND(AVG(CASE WHEN m.season='2014/2015' AND m.home_goal = m.away_goal THEN 1
+			WHEN m.season='2014/2015' AND m.home_goal != m.away_goal THEN 0
+			END), 2) AS pct_ties_2014_2015
+FROM country AS c
+LEFT JOIN matches AS m
+	ON c.id = m.country_id
+GROUP BY country;
+
+-- Count of home wins, away wins, and ties in each country
+
+SELECT 
+    c.name AS country,
+    -- Count the home wins, away wins, and ties in each country
+	AVG(CASE WHEN m.home_goal > m.away_goal THEN m.id 
+        END) AS home_wins,
+	AVG(CASE WHEN m.home_goal < m.away_goal THEN m.id 
+        END) AS away_wins,
+	AVG (CASE WHEN m.home_goal = m.away_goal THEN m.id 
+        END) AS ties
+FROM country AS c
+LEFT JOIN matches AS m
+	ON c.id = m.country_id
+GROUP BY country;
+
+-- Identify the number of matches played by each country during the three different seasons.
 
 -- Method #1: Using CASE (all dialects)
 
@@ -36,6 +69,30 @@ SELECT
 FROM country AS c 
 LEFT JOIN match AS m 
 	ON c.id=m.country_id
+GROUP BY country;
+
+-- Now total only the home games within each country
+
+SELECT
+    c.name AS country,
+    SUM(
+        CASE
+            WHEN home_goal>away_goal AND season='2012/2013' THEN 1
+            ELSE 0
+        END) AS home_wins_2012_2013,
+    SUM(
+        CASE
+            WHEN home_goal>away_goal AND season='2013/2014' THEN 1
+            ELSE 0
+        END) AS home_wins_2013_2014,
+    SUM(
+        CASE
+            WHEN home_goal>away_goal AND season='2014/2015' THEN 1
+            ELSE 0
+        END) AS home_wins_2014_2015
+FROM match AS m 
+LEFT JOIN country AS c 
+    ON m.country_id=c.id
 GROUP BY country
 
 
