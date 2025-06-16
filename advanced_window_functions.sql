dc36c7c134a6f0407c43d54b30bcf762cdd2dbb2
Katie Romrell <kmromrell@gmail.com>
2025-06-16 12:09:35 -0700

    Ranking athletes with most medals

--- advanced_window_functions.sql
+++ advanced_window_functions.sql
@@ -1,4 +1,36 @@
 /* These queries are performed in PostgreSQL using a Summer Olympics dataset, which contains the results of the games between 1896 and 2012. The first Summer Olympics were held in 1896, the second in 1900, and so on. */
+-- Identify and rank the athletes who have earned the most medals in the summer olympics
+
+-- Method #1: My way (subquery in FROM, RANK)
+
+SELECT 
+  athlete,
+  medals,
+  RANK() OVER(ORDER BY medals DESC)
+FROM (
+  SELECT 
+    athlete,
+    COUNT(medal) AS medals
+  FROM summer_medals 
+  GROUP BY athlete
+) AS athlete_medals;
+
+-- Method #2: DataCamp method (CTE, ROW_NUMBER)
+
+WITH athlete_medals AS (
+  SELECT 
+    athlete,
+    count(medal) AS medals
+  FROM summer_medals
+  GROUP BY athlete
+)
+
+SELECT 
+  athlete,
+  ROW_NUMBER() OVER(ORDER BY medals DESC) AS row_n,
+  medals
+FROM athlete_medals;
+
 -- Write a query to number each distinct summer olympics in reverse order.
 
 SELECT
