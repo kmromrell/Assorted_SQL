b5f070868a085007a6d5624b15a7524c90ec8cd6
Katie Romrell <kmromrell@gmail.com>
2025-06-16 10:57:45 -0700

    Create advanced_window_functions.sql

---
+++ advanced_window_functions.sql
@@ -0,0 +1,22 @@
+/* These queries are performed in PostgreSQL using a Summer Olympics dataset, which contains the results of the games between 1896 and 2012. The first Summer Olympics were held in 1896, the second in 1900, and so on. */
+
+
+-- Write a query to number each distinctive summer olympics so far.
+
+SELECT DISTINCT
+  year,
+  ROW_NUMBER() OVER () AS row_n
+FROM (
+  SELECT DISTINCT year
+  FROM summer_medals
+  ORDER BY year
+) AS years
+ORDER BY year ASC;
+
+-- Write a query to add row numbers to each row in the table.
+
+SELECT
+  *,
+ ROW_NUMBER() OVER() AS row_n
+FROM summer_medals
+ORDER BY row_n ASC;
