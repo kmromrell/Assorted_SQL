53590a236987e0dcaf11867032786f04cc68941c
Katie Romrell <kmromrell@gmail.com>
2025-06-16 12:09:16 -0700

    Reverse order for ROW_NUMBER

--- advanced_window_functions.sql
+++ advanced_window_functions.sql
@@ -1,4 +1,14 @@
 /* These queries are performed in PostgreSQL using a Summer Olympics dataset, which contains the results of the games between 1896 and 2012. The first Summer Olympics were held in 1896, the second in 1900, and so on. */
+-- Write a query to number each distinct summer olympics in reverse order.
+
+SELECT
+  year,
+  ROW_NUMBER() OVER (ORDER BY year DESC) AS Row_N
+FROM (
+  SELECT DISTINCT year
+  FROM summer_medals
+) AS years
+ORDER BY year;
 
 
 -- Write a query to number each distinctive summer olympics so far.
