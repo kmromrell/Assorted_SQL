80ce6582e8cf5e122cad87b6203ff49b59049d19
Katie Romrell <kmromrell@gmail.com>
2025-06-20 10:45:04 -0700

    Using intervals to create new columns

--- manipulating_data_structure.sql
+++ manipulating_data_structure.sql
@@ -1,3 +1,12 @@
+--Select the rental date and return date from the rental table. Add an INTERVAL of 3 days to the rental_date to calculate the expected return date`.
+
+SELECT 
+	rental_date,
+	rental_date+INTERVAL '3 DAY' AS expected_return_date,
+	return_date 
+FROM 
+	rental 
+
 -- Select the column name and data type from the INFORMATION_SCHEMA.COLUMNS system database. Limit results to only include the customer table.
 
 SELECT 
