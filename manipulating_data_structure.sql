875e88d22c4fa6b32c9ecda1076f0a7e37839c05
Katie Romrell <kmromrell@gmail.com>
2025-06-20 10:52:03 -0700

    Searching arrays for special features

--- manipulating_data_structure.sql
+++ manipulating_data_structure.sql
@@ -1,3 +1,39 @@
+
+
+
+
+-- Use the contains operator to match the text Deleted Scenes in the special_features column.
+
+SELECT 
+  title, 
+  special_features 
+FROM film 
+WHERE special_features @> ARRAY['Deleted Scenes'];
+
+-- Match 'Trailers' in any index of the special_features ARRAY regardless of position.
+
+SELECT
+  title, 
+  special_features 
+FROM film 
+WHERE 'Trailers' = ANY(special_features);
+
+-- Now let's select all films that have Deleted Scenes in the second index of the special_features ARRAY.
+
+SELECT 
+  title, 
+  special_features 
+FROM film
+WHERE special_features[2] = 'Deleted Scenes';
+
+-- Select all films that have a special feature Trailers by filtering on the first index of the special_features ARRAY.
+
+SELECT 
+  title, 
+  special_features 
+FROM film
+WHERE special_features[1] = 'Trailers';
+
 --Select the rental date and return date from the rental table. Add an INTERVAL of 3 days to the rental_date to calculate the expected return date`.
 
 SELECT 
